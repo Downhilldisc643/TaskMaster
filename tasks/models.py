81b4b3a7375e8b7b896c39180914185d5af4ae7f
Downhilldisc643 <borisnikolov10@icloud.com>
2025-05-06 16:43:48 +0300

    създадох Task model с title, completed, priority, и due_date

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES=[
+        ('Low', 'Low'),
+        ('Medium', 'Medium'),
+        ('High', 'High'),
+    ]
+
+    title=models.CharField(max_length=200)
+    completed=models.BooleanField(default=False)
+    priority=models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='Medium')
+    due_date=models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
