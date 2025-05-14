1c93613cd644d9ee18321ff14d30741db1617e2b
Downhilldisc643 <borisnikolov10@icloud.com>
2025-05-14 16:42:40 +0300

    Създадох Task form, view, и template за добавяне и гледане на задачи

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+def task_list(request):
+    tasks=Task.objects.all().order_by('-priority', 'due_date') 
+    form=TaskForm()
+
+    if request.method=='POST':
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context={'tasks': tasks, 'form': form}
+    return render(request, 'tasks/task_list.html', context)
